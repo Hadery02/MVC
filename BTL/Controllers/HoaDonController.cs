dbcd7e9c21b8555d92b491b54a2b3e56236cc4d8
quan <minhquanbg925@gmail.com>
2023-12-06 14:26:22 +0700

    up

---
+++ BTL/Controllers/HoaDonController.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BTL.Data;
+using BTL.Models;
+
+namespace BTL.Controllers
+{
+    public class HoaDonController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HoaDonController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HoaDon
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.HoaDon.Include(h => h.KhachHang).Include(h => h.Phong);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: HoaDon/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoaDon = await _context.HoaDon
+                .Include(h => h.KhachHang)
+                .Include(h => h.Phong)
+                .FirstOrDefaultAsync(m => m.MaHoaDon == id);
+            if (hoaDon == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoaDon);
+        }
+
+        // GET: HoaDon/Create
+        public IActionResult Create()
+        {
+            ViewData["MaKhachHang"] = new SelectList(_context.KhachHang, "MaKhachHang", "MaKhachHang");
+            ViewData["MaPhong"] = new SelectList(_context.Phong, "MaPhong", "MaPhong");
+            return View();
+        }
+
+        // POST: HoaDon/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaHoaDon,MaKhachHang,MaPhong,ThanhTien,NgayThanhToan")] HoaDon hoaDon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hoaDon);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaKhachHang"] = new SelectList(_context.KhachHang, "MaKhachHang", "MaKhachHang", hoaDon.MaKhachHang);
+            ViewData["MaPhong"] = new SelectList(_context.Phong, "MaPhong", "MaPhong", hoaDon.MaPhong);
+            return View(hoaDon);
+        }
+
+        // GET: HoaDon/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoaDon = await _context.HoaDon.FindAsync(id);
+            if (hoaDon == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaKhachHang"] = new SelectList(_context.KhachHang, "MaKhachHang", "MaKhachHang", hoaDon.MaKhachHang);
+            ViewData["MaPhong"] = new SelectList(_context.Phong, "MaPhong", "MaPhong", hoaDon.MaPhong);
+            return View(hoaDon);
+        }
+
+        // POST: HoaDon/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaHoaDon,MaKhachHang,MaPhong,ThanhTien,NgayThanhToan")] HoaDon hoaDon)
+        {
+            if (id != hoaDon.MaHoaDon)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hoaDon);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HoaDonExists(hoaDon.MaHoaDon))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaKhachHang"] = new SelectList(_context.KhachHang, "MaKhachHang", "MaKhachHang", hoaDon.MaKhachHang);
+            ViewData["MaPhong"] = new SelectList(_context.Phong, "MaPhong", "MaPhong", hoaDon.MaPhong);
+            return View(hoaDon);
+        }
+
+        // GET: HoaDon/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoaDon = await _context.HoaDon
+                .Include(h => h.KhachHang)
+                .Include(h => h.Phong)
+                .FirstOrDefaultAsync(m => m.MaHoaDon == id);
+            if (hoaDon == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoaDon);
+        }
+
+        // POST: HoaDon/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hoaDon = await _context.HoaDon.FindAsync(id);
+            if (hoaDon != null)
+            {
+                _context.HoaDon.Remove(hoaDon);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HoaDonExists(int id)
+        {
+            return _context.HoaDon.Any(e => e.MaHoaDon == id);
+        }
+    }
+}
---
+++ BTL/Migrations/20231206071921_Create_table_HoaDon.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BTL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_table_HoaDon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HoaDons",
+                columns: table => new
+                {
+                    MaHoaDon = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MaKhachHang = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaPhong = table.Column<int>(type: "INTEGER", nullable: false),
+                    ThanhTien = table.Column<int>(type: "INTEGER", nullable: false),
+                    NgayThanhToan = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HoaDons", x => x.MaHoaDon);
+                    table.ForeignKey(
+                        name: "FK_HoaDons_KhachHang_MaKhachHang",
+                        column: x => x.MaKhachHang,
+                        principalTable: "KhachHang",
+                        principalColumn: "MaKhachHang",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HoaDons_Phong_MaPhong",
+                        column: x => x.MaPhong,
+                        principalTable: "Phong",
+                        principalColumn: "MaPhong",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HoaDons_MaKhachHang",
+                table: "HoaDons",
+                column: "MaKhachHang");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HoaDons_MaPhong",
+                table: "HoaDons",
+                column: "MaPhong");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HoaDons");
+        }
+    }
+}
